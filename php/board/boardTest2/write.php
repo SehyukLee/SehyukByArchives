def3192541654a899b73de2e55fceb366bf0c045
SehyukLee <33459130+SehyukLee@users.noreply.github.com>
2018-08-30 20:31:59 +0900

    Update write.php

--- php/board/boardTest2/write.php
+++ php/board/boardTest2/write.php
@@ -1,23 +1,28 @@
 <?php
-    $title = isset($_POST['title']) ? $_POST['title'] : false;
-    $text = isset($_POST['text']) ? $_POST['text'] : false;
+    $title = isset($_POST['title']) ? $_POST['title'] : false;      // 게시글 제목
+    $text = isset($_POST['text']) ? $_POST['text'] : false;         // 게시글 내용
+    
     $text = str_replace(" ", "&nbps;", $text);
     $text = nl2br($text);
     $text = htmlentities($text, ENT_QUOTES);
-    $today = date("Y-m-d H:i:s");
+    // 문자열 처리
+
+    $today = date("Y-m-d H:i:s");   // 현재 날짜
 
     @$db_con = mysql_connect("localhost", "root", "autoset");
     mysql_select_db("sehyuk_board");
+    // DB연결
 
-    $id = $_SESSION['id'];
-    $name = $_SESSION['name'];
+    $id = $_SESSION['id'];          // 로그인한 유저 아이디
+    $name = $_SESSION['name'];      // 로그인한 유저 이름
 
     mysql_query("insert into board values ('', 0, '$id', '$name', '$title', '$text', 0, '$today')");
-    mysql_close();
+    // 게시글 저장
+
+    mysql_close();                  // DB연결 종료
 
     echo "<script>";
     echo "window.location = 'board_list.php'";
     echo "</script>";
-
-    mysql_close();
+    // 게시판 리스트로 페이지 이동
 ?>
