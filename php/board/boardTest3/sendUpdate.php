f4b540f0cf0786b06493b80b711e942929a85970
SehyukLee <33459130+SehyukLee@users.noreply.github.com>
2018-08-30 21:52:19 +0900

    Update sendUpdate.php

--- php/board/boardTest3/sendUpdate.php
+++ php/board/boardTest3/sendUpdate.php
@@ -1,14 +1,16 @@
 <?php
-    $update_id = isset($_POST['update_id']) ? $_POST['update_id'] : null;
-    $subject = isset($_POST['notNull']) ? $_POST['notNull'] : null;
-    $content = isset($_POST['content']) ? $_POST['content'] : null;
+    $update_id = isset($_POST['update_id']) ? $_POST['update_id'] : null;   // 수정 할 게시글 아이디
+    $subject = isset($_POST['notNull']) ? $_POST['notNull'] : null;         // 수정 할 게시글 제목
+    $content = isset($_POST['content']) ? $_POST['content'] : null;         // 수정 할 게시글 내용
 
     @$db_con = mysql_connect("localhost", "root", "autoset");
     mysql_select_db("ycj_test");
+    // DB연결
 
     mysql_query("update ycj_first_board set subject = '$subject', contents = '$content' where board_id = $update_id");
+    // 게시글 수정
 
-    mysql_close($db_con);
+    mysql_close($db_con);   // DB연결 종료
 ?>
 <script>
     alert("수정 되었습니다.");
