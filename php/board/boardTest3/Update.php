a52bd311bd47d8454cda3318d51312e92eebb5aa
SehyukLee <33459130+SehyukLee@users.noreply.github.com>
2018-08-30 21:34:40 +0900

    Update Update.php

--- php/board/boardTest3/Update.php
+++ php/board/boardTest3/Update.php
@@ -20,19 +20,24 @@
             document.getElementById("form").setAttribute("action", "sendUpdate.php");
         }
     }
+    // 제목 유무 확인
 </script>
 <body>
 <?php
-    $view_id = isset($_GET['id']) ? $_GET['id'] : null;
+    $view_id = isset($_GET['id']) ? $_GET['id'] : null;         // 게시글 아이디
 
     @$db_con = mysql_connect("localhost", "root", "autoset");
     mysql_select_db("ycj_test");
+    // DB연결
 
     $result = mysql_query("select subject, contents from ycj_first_board where board_id = $view_id");
     $result = mysql_fetch_row($result);
+    // 게시글 내용 검색
+    
     $result[1] = htmlspecialchars_decode($result[1]);
     $result[1] = strip_tags($result[1]);
-
+    // 문자열 처리
+    
     echo "<form method=\"post\" id=\"form\">";
     echo "제목 <input type=\"text\" size=\"62px\" id=\"subject\" name=\"notNull\" value='$result[0]'>";
     echo "<br>내용<br>";
@@ -41,12 +46,13 @@
     echo "</textarea><br>";
     echo "<input type=\"submit\" value=\"수정완료\" onclick=\"checkSubject(this.id)\" id='$view_id'>";
     echo "</form>";
-
+    
     echo "<form action=\"list.php\">";
     echo "<input type=\"submit\" value=\"취소\">";
     echo "</form>";
+    // 게시글 내용 출력
 
-    mysql_close($db_con);
+    mysql_close($db_con);   // DB 연결 종료
 ?>
 </body>
 </html>
