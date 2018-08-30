065681c4c1484d6b0e8396cd137a59ea16eb088a
SehyukLee <33459130+SehyukLee@users.noreply.github.com>
2018-08-30 20:06:25 +0900

    Update logout.php

--- php/board/boardTest2/logout.php
+++ php/board/boardTest2/logout.php
@@ -1,7 +1,8 @@
 <?php
-    session_destroy();
+    session_destroy();      // 세션 삭제
 
     echo "<script>";
     echo "window.location = 'board_list.php'";
     echo "</script>";
+    // 게시판 리스트로 페이지 이동
 ?>
