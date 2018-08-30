5acc0d54fc8d3061bab54ab2f5797752033d8c8e
SehyukLee <33459130+SehyukLee@users.noreply.github.com>
2018-08-30 20:11:32 +0900

    Update remove_board.php

--- php/board/boardTest2/remove_board.php
+++ php/board/boardTest2/remove_board.php
@@ -1,22 +1,31 @@
 <?php
-    $board_id = isset($_GET['board_id']) ? $_GET['board_id'] : false;
-    $under_id = isset($_POST['under_id']) ? $_POST['under_id'] : false;
+    $board_id = isset($_GET['board_id']) ? $_GET['board_id'] : false;       // 클릭한 게시판 아이디
+    $under_id = isset($_POST['under_id']) ? $_POST['under_id'] : false;     // 클릭한 댓글 아이디
 
     @$db_con = mysql_connect("localhost", "root", "autoset");
     mysql_select_db("sehyuk_board");
+    // DB 연결
 
     if ($board_id) {
+        // 게시글을 삭제할 경우
+        
         mysql_query("delete from board where board_id = $board_id");
-
-        mysql_close();
+        // 게시글 삭제
+        
+        mysql_close();  // DB 연결 종료
 
         echo "<script>";
         echo "window.location = 'board_list.php'";
         echo "</script>";
+        // 게시판 리스트로 페이지 이동
     }
     else {
+        // 댓글을 삭제할 경우
+        
         mysql_query("delete from board where board_id = $under_id");
+        // 댓글 삭제
 
         mysql_close();
+        // DB 연결 종료
     }
  ?>
