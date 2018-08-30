9402171417a45fd409e22757d46d6d5ea0149308
SehyukLee <33459130+SehyukLee@users.noreply.github.com>
2018-08-30 21:43:42 +0900

    Update find.php

--- php/board/boardTest3/find.php
+++ php/board/boardTest3/find.php
@@ -9,6 +9,7 @@
         border: 1px solid black;
         border-collapse: collapse;
     }
+    // 테이블 스타일
 </style>
 <body>
 <h1 align="center">
@@ -22,21 +23,27 @@
         <td class="table">작성 시간</td>
     </tr>
     <?php
-    $inputPage = isset($_GET['page']) ? $_GET['page'] : 1;
-    $page = ($inputPage - 1) * 5;
+    $inputPage = isset($_GET['page']) ? $_GET['page'] : 1;      // 페이지 번호
+    $page = ($inputPage - 1) * 5;                               // 페이지 계산
 
-    $findText = isset($_GET['find']) ? $_GET['find'] : null;
+    $findText = isset($_GET['find']) ? $_GET['find'] : null;    // 찾는 내용
 
     @$db_con = mysql_connect("localhost", "root", "autoset");
     mysql_select_db("ycj_test");
+    // DB연결
 
     $findList = mysql_query("select board_id, subject, hits, reg_date from ycj_first_board where (subject like '%$findText%' or contents like '%$findText%') and board_pid = 0 order by reg_date desc limit $page, 5");
-    $row = mysql_num_rows($findList);
-    $field = mysql_num_fields($findList);
+    // 찾는 내용 검색
+    
+    $row = mysql_num_rows($findList);       // 출력할 열의 수
+    $field = mysql_num_fields($findList);   // 출력할 행의 수
 
     $All = mysql_query("select count(board_id) from ycj_first_board where subject like '%$findText%' or contents like '%$findText%' and board_pid = 0");
     $All = mysql_fetch_row($All);
+    // 총 개수 확인
+    
     $pageCount = ceil($All[0] / 5);
+    // 총 개수 정리
 
     for ($i = 0; $i < $row; $i++) {
         $resultArr = mysql_fetch_row($findList);
@@ -65,7 +72,11 @@
         echo "</tr>";
     }
     echo "</table>";
+    // 내용 출력
+    
     echo "<br>";
+    
+    // 페이지 네이션
     echo "<div align='center'>";
 
     $nextPage = $inputPage;
@@ -160,8 +171,11 @@
 
         echo "</div>";
     }
+    // 페이지 네이션
 
     if (isset($_SESSION['userid'])) {
+        // 로그인 했을 경우
+        
         echo $_SESSION['name']."님이 로그인하셨습니다.<br>";
         echo "<form action=\"logout.php\">";
         echo "<input type=\"submit\" value=\"로그아웃\">";
@@ -173,12 +187,14 @@
         echo "</div>";
     }
     else {
+        // 로그인 하지 않았을 경우
+        
         echo "<form action=\"log_page.html\">";
         echo "<input type=\"submit\" value=\"로그인\">";
         echo "</form>";
     }
 
-    mysql_close($db_con);
+    mysql_close($db_con);   // DB연결 종료
     ?>
 <div align="center">
     <form action="find.php" method="get">
@@ -191,6 +207,7 @@
         function returnList() {
             location.href="list.php";
         }
+        // 원래 페이지 이동
     </script>
 </body>
 </html>
