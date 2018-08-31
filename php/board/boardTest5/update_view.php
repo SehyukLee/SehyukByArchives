81318278cb5f08d804e09db0b9a9291e2ac85ee3
SehyukLee <33459130+SehyukLee@users.noreply.github.com>
2018-08-31 11:21:09 +0900

    Update update_view.php

--- php/board/boardTest5/update_view.php
+++ php/board/boardTest5/update_view.php
@@ -1,13 +1,17 @@
 <?php
-    $login_user_id = isset($_POST["userid"]) ? $_POST["userid"] : false;
-    $board_idNum = isset($_POST["board_idNum"]) ? $_POST["board_idNum"] : false;
+    $login_user_id = isset($_POST["userid"]) ? $_POST["userid"] : false;            // 유저 아이디
+    $board_idNum = isset($_POST["board_idNum"]) ? $_POST["board_idNum"] : false;    // 게시글 아이디
 
     if (!$login_user_id) {
+        // 로그인하지 않았을 경우
+        
         echo "no userid data";
         exit();
     }
 
     if (!$board_idNum) {
+        // 게시글 번호가 없을 경우
+        
         echo "no board_idNum";
         exit();
     }
@@ -18,24 +22,28 @@
         const USER_PASSWD = "autoset";
         const DB_NAME     = "shl_board";
     }
+    // DB연결에 사용할 값 정리
 
     class board_DB {
         function viewSelect ($userid, $boardNum) {
             $db_con = new mysqli(DB_info::IP_ADRESS, DB_info::USER_NAME, DB_info::USER_PASSWD, DB_info::DB_NAME);
-
+            // DB연결
+            
             if (!$db_con) {
                 echo "DB connect fail";
                 exit();
             }
 
             $view = $db_con->query("select board_name, board_content, user_id from board where board_idNum=$boardNum");
-
+            // 게시글 검색
+            
             if (!$view) {
                 echo "board select query fail";
             }
 
             $view_re = $view->fetch_array(MYSQLI_NUM);
-
+            
+            // 게시글 출력
             echo "<form action='save_updateView.php?userid=$userid&board_idNum=$boardNum' method='post'>
                     <table align=\"center\">
                         <thead>
@@ -63,9 +71,10 @@
                             <input type=\"button\" value=\"취소\" onclick=\"backList(2)\">
                        </div>
                        </form>";
+            // 게시글 출력
         }
     }
 
-$shl_board_db = new board_DB();
-$shl_board_db->viewSelect($login_user_id, $board_idNum);
+$shl_board_db = new board_DB();                             // DB연결 객체 생성
+$shl_board_db->viewSelect($login_user_id, $board_idNum);    // 게시글 
 ?>
