55c861f32e32e64baf4f06e75a51258aec1eb292
SehyukLee <33459130+SehyukLee@users.noreply.github.com>
2018-08-31 10:31:13 +0900

    Update board_content_select.php

--- php/board/boardTest5/board_content_select.php
+++ php/board/boardTest5/board_content_select.php
@@ -1,13 +1,17 @@
 <?php
-    $login_user_id = isset($_POST["userid"]) ? $_POST["userid"] : false;
-    $board_idNum = isset($_POST["board_idNum"]) ? $_POST["board_idNum"] : false;
+    $login_user_id = isset($_POST["userid"]) ? $_POST["userid"] : false;            // 유저 아이디
+    $board_idNum = isset($_POST["board_idNum"]) ? $_POST["board_idNum"] : false;    // 게시글 아이디
 
     if (!$login_user_id) {
+        // 유저 아이디가 없을 경우
+        
         echo "no userid data";
         exit();
     }
 
     if (!$board_idNum) {
+        // 게시글 아이디가 없을 경우
+        
         echo "no board_idNum";
         exit();
     }
@@ -18,24 +22,28 @@
         const USER_PASSWD = "autoset";
         const DB_NAME     = "shl_board";
     }
+    // DB연결에 사용할 값 정리
 
     class board_DB {
         function veiwSelect ($userid, $boardNum) {
             $db_con = new mysqli(DB_info::IP_ADRESS, DB_info::USER_NAME, DB_info::USER_PASSWD, DB_info::DB_NAME);
-
+            // DB연결
+            
             if (!$db_con) {
                 echo "DB connect fail";
                 exit();
             }
 
             $view = $db_con->query("select board_name, board_content, user_id from board where board_idNum=$boardNum");
-
+            // 게시글의 내용 검색
+            
             if (!$view) {
                 echo "board select query fail";
             }
 
             $view_re = $view->fetch_array(MYSQLI_NUM);
-
+            
+            // 게시글 내용 출력
             echo "<table align=\"center\">
                         <thead>
                             <tr align=\"center\">
@@ -55,8 +63,11 @@
                             </tr>
                         </tbody>
                        </table>";
+            // 게시글 내용 출력
 
             if ($view_re[2] == $userid) {
+                // 로그인 했을 경우
+                
                 echo "<div align=\"center\">
                             <input type=\"button\" value=\"수정하기\" onclick=\"view_update()\">
                             <input type=\"button\" value=\"삭제하기\" onclick=\"view_delete()\">
@@ -64,6 +75,8 @@
                        </div>";
             }
             else {
+                // 로그인 하지 않았을 경우
+                
                 echo "<div align=\"center\">
                             <input type=\"button\" value=\"취소\" onclick=\"backList(1)\">
                        </div>";
